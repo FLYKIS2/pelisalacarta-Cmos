7badbb49b91f8ac4a1ded39f5157ad08971eab33
CmosGit <francm0785@gmail.com>
2015-11-02 18:50:08 +0100

    Añadida opción torrent

---
+++ channels/tengourl.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------
+# pelisalacarta - XBMC Plugin
+# Canal para ver un vídeo conociendo su URL
+# http://blog.tvalacarta.info/plugin-xbmc/pelisalacarta/
+#------------------------------------------------------------
+import urlparse,urllib2,urllib,re
+import os
+import sys
+
+from core import scrapertools
+from core import config
+from core import logger
+from core.item import Item
+from servers import servertools
+
+__channel__ = "tengourl"
+__category__ = "G"
+__type__ = "generic"
+__title__ = "tengourl"
+__language__ = ""
+
+DEBUG = config.get_setting("debug")
+
+def isGeneric():
+    return True
+
+def mainlist(item):
+    logger.info("[tengourl.py] mainlist")
+
+    itemlist = []
+    itemlist.append( Item(channel=__channel__, action="search", title="Entra aquí y teclea la URL"))
+    itemlist.append( Item(channel=__channel__, action="search", title="Archivos Torrent", extra="torrent"))
+
+    return itemlist
+
+# Al llamarse "search" la función, el launcher pide un texto a buscar y lo añade como parámetro
+def search(item,texto):
+    logger.info("[tengourl.py] search texto="+texto)
+    
+    if not texto.startswith("http://"):
+        texto = "http://"+texto
+    
+    itemlist = []
+    if item.extra == "torrent":
+        itemlist.append( Item(channel=__channel__, title = "Torrent", action="play", url=texto, server="torrent", folder=False) )
+    else:
+        itemlist = servertools.find_video_items(data=texto)
+        for item in itemlist:
+            item.channel=__channel__
+            item.action="play"
+
+    if len(itemlist)==0:
+        itemlist.append( Item(channel=__channel__, action="search", title="No hay ningún vídeo compatible en esa URL"))
+    
+    return itemlist
+
+def test():
+    return True
